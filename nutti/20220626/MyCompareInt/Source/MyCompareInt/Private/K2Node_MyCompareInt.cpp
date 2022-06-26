c6a52b8286cc88fc1376b4a1f4dbc34f5a7ff723
nutti <nutti.metro@gmail.com>
2022-06-26 15:48:08 +0900

    Fix

--- nutti/20220626/MyCompareInt/Source/MyCompareInt/Private/K2Node_MyCompareInt.cpp
+++ nutti/20220626/MyCompareInt/Source/MyCompareInt/Private/K2Node_MyCompareInt.cpp
@@ -29,11 +29,11 @@ public:
 
     virtual void RegisterNets(FKismetFunctionContext& Context, UEdGraphNode* Node) override
     {
-        UK2Node_MyCompareInt* MyCompareIntNode = CastChecked<UK2Node_MyCompareInt>(Node);
-
         FNodeHandlingFunctor::RegisterNets(Context, Node);
 
         {
+            // ライブラリ関数「UKismetMathLibrary::GreaterEqual_IntInt」の実行結果を格納する
+            // Boolean型の一時変数を作成する
             FBPTerminal* BoolTerm = Context.CreateLocalTerminal();
             BoolTerm->Type.PinCategory = UEdGraphSchema_K2::PC_Boolean;
             BoolTerm->Source = Node;
@@ -41,40 +41,47 @@ public:
             BoolTermMap.Add(TEXT("IsGreaterEqual"), BoolTerm);
         }
         {
+            // ライブラリ関数「UKismetMathLibrary::LessEqual_IntInt」の実行結果を格納する
+            // Boolean型の一時変数を作成する
             FBPTerminal* BoolTerm = Context.CreateLocalTerminal();
             BoolTerm->Type.PinCategory = UEdGraphSchema_K2::PC_Boolean;
             BoolTerm->Source = Node;
             BoolTerm->Name = Context.NetNameMap->MakeValidName(Node, TEXT("IsLessEqual"));
             BoolTermMap.Add(TEXT("IsLessEqual"), BoolTerm);
         }
-
     }
 
     virtual void Compile(FKismetFunctionContext& Context, UEdGraphNode* Node) override
     {
         UK2Node_MyCompareInt* MyCompareIntNode = CastChecked<UK2Node_MyCompareInt>(Node);
 
+        // 出力側の実行ピンを取得する
         UEdGraphPin* GreaterPin = MyCompareIntNode->FindPin(GreaterPinName);
         UEdGraphPin* EqualPin = MyCompareIntNode->FindPin(EqualPinName);
         UEdGraphPin* LessPin = MyCompareIntNode->FindPin(LessPinName);
 
+        // 一時変数を取得する
         FBPTerminal* IsGreaterEqualTerm = BoolTermMap.FindRef(TEXT("IsGreaterEqual"));
         FBPTerminal* IsLessEqualTerm = BoolTermMap.FindRef(TEXT("IsLessEqual"));
 
+        // 入力ピン「A」の変数を取得する
         UEdGraphPin* APin = MyCompareIntNode->FindPin(APinName);
         UEdGraphPin* ANet = FEdGraphUtilities::GetNetFromPin(APin);
         FBPTerminal* ATerm = Context.NetMap.FindRef(ANet);
 
+        // 入力ピン「B」の変数を取得する
         UEdGraphPin* BPin = MyCompareIntNode->FindPin(BPinName);
         UEdGraphPin* BNet = FEdGraphUtilities::GetNetFromPin(BPin);
         FBPTerminal* BTerm = Context.NetMap.FindRef(BNet);
 
         {
+            // ライブラリ関数「UKismetMathLibrary::GreaterEqual_IntInt」のUFunctionを取得する
             UEdGraphPin* FunctionPin = MyCompareIntNode->FindPin(TEXT("GreaterEqual_IntInt"));
             FBPTerminal* FunctionContext = Context.NetMap.FindRef(FunctionPin);
             UClass* FunctionClass = Cast<UClass>(FunctionPin->PinType.PinSubCategoryObject.Get());
             UFunction* FunctionPtr = FindUField<UFunction>(FunctionClass, FunctionPin->PinName);
 
+            // ライブラリ関数を呼び出すステートメントを作成する
             FBlueprintCompiledStatement& CallFuncStatement = Context.AppendStatementForNode(MyCompareIntNode);
             CallFuncStatement.Type = KCST_CallFunction;
             CallFuncStatement.FunctionToCall = FunctionPtr;
@@ -84,19 +91,21 @@ public:
             CallFuncStatement.RHS.Add(ATerm);
             CallFuncStatement.RHS.Add(BTerm);
 
+            // ライブラリ関数の呼び出し結果ががFalseのときに、実行ピン「LessPin」に実行制御が移るステートメントを作成する
             FBlueprintCompiledStatement& GotoStatement = Context.AppendStatementForNode(MyCompareIntNode);
             GotoStatement.Type = KCST_GotoIfNot;
             GotoStatement.LHS = IsGreaterEqualTerm;
-
             Context.GotoFixupRequestMap.Add(&GotoStatement, LessPin);
         }
 
         {
+            // ライブラリ関数「UKismetMathLibrary::LessEqual_IntInt」のUFunctionを取得する
             UEdGraphPin* FunctionPin = MyCompareIntNode->FindPin(TEXT("LessEqual_IntInt"));
             FBPTerminal* FunctionContext = Context.NetMap.FindRef(FunctionPin);
             UClass* FunctionClass = Cast<UClass>(FunctionPin->PinType.PinSubCategoryObject.Get());
             UFunction* FunctionPtr = FindUField<UFunction>(FunctionClass, FunctionPin->PinName);
 
+            // ライブラリ関数を呼び出すステートメントを作成する
             FBlueprintCompiledStatement& CallFuncStatement = Context.AppendStatementForNode(MyCompareIntNode);
             CallFuncStatement.Type = KCST_CallFunction;
             CallFuncStatement.FunctionToCall = FunctionPtr;
@@ -106,13 +115,14 @@ public:
             CallFuncStatement.RHS.Add(ATerm);
             CallFuncStatement.RHS.Add(BTerm);
 
+            // ライブラリ関数の呼び出し結果がFalseのときに、実行ピン「GreaterPin」に実行制御が移るステートメントを作成する
             FBlueprintCompiledStatement& GotoStatement = Context.AppendStatementForNode(MyCompareIntNode);
             GotoStatement.Type = KCST_GotoIfNot;
             GotoStatement.LHS = IsLessEqualTerm;
-
             Context.GotoFixupRequestMap.Add(&GotoStatement, GreaterPin);
         }
 
+        // いずれの条件も満たさない場合は、無条件で実行ピン「EqualPin」に実行制御が移るステートメントを作成する
         GenerateSimpleThenGoto(Context, *MyCompareIntNode, EqualPin);
     }
 };
@@ -127,6 +137,7 @@ void UK2Node_MyCompareInt::AllocateDefaultPins()
     CreatePin(EGPD_Output, UEdGraphSchema_K2::PC_Exec, LessPinName);
 
     {
+        // ライブラリ関数「UKismetMathLibrary::GreaterEqual_IntInt」
         UEdGraphPin* FunctionPin = CreatePin(EGPD_Input, UEdGraphSchema_K2::PC_Object, UKismetMathLibrary::StaticClass(), TEXT("GreaterEqual_IntInt"));
         FunctionPin->bDefaultValueIsReadOnly = true;
         FunctionPin->bNotConnectable = true;
@@ -169,7 +180,7 @@ void UK2Node_MyCompareInt::AllocateDefaultPins()
 
 FText UK2Node_MyCompareInt::GetTooltipText() const
 {
-    return LOCTEXT("MyCompareInt_Tooltip", "MyCompareInt\nCompare integer and ");
+    return LOCTEXT("MyCompareInt_Tooltip", "MyCompareInt\nCompare one integer to another and indicate if it is greater, less than or the same.");
 }
 
 FLinearColor UK2Node_MyCompareInt::GetNodeTitleColor() const
@@ -202,7 +213,7 @@ void UK2Node_MyCompareInt::GetMenuActions(FBlueprintActionDatabaseRegistrar& Act
 
 FText UK2Node_MyCompareInt::GetMenuCategory() const
 {
-    return FEditorCategoryUtils::GetCommonCategory(FCommonEditorCategory::Development);
+    return FEditorCategoryUtils::GetCommonCategory(FCommonEditorCategory::FlowControl);
 }
 
 FNodeHandlingFunctor* UK2Node_MyCompareInt::CreateNodeHandler(class FKismetCompilerContext& CompilerContext) const
